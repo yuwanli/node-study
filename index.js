940b284f750249682e1540e177f4109897d71c03
余万利 <604073367@qq.com>
2018-03-19 17:11:23 +0800

    [edit]

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-require('colors');
-console.log('smasing node'.rainbow);
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
